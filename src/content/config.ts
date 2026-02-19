27603a47b31127fbd5a3447046848067a5721b4a
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-02-19 17:46:32 +0000

    ✨ FEAT: Update content guidelines and improve image handling

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,36 +3,38 @@ import { defineCollection, z } from 'astro:content';
 // Define the blog collection
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z
-      .string()
-      .datetime({ offset: true })
-      .or(z.string().regex(/^\d{4}-\d{2}-\d{2}$/)),
-    tags: z.array(z.string()).optional(),
-    slug: z.string().optional(),
-    cover: z.string().optional(),
-    draft: z.boolean().optional().default(false),
-  }),
+  schema: ({ image: img }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z
+        .string()
+        .datetime({ offset: true })
+        .or(z.string().regex(/^\d{4}-\d{2}-\d{2}$/)),
+      tags: z.array(z.string()).optional(),
+      slug: z.string().optional(),
+      cover: img().optional(),
+      draft: z.boolean().optional().default(false),
+    }),
 });
 
 // Define the projects collection
 const projects = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z
-      .string()
-      .datetime({ offset: true })
-      .or(z.string().regex(/^\d{4}-\d{2}-\d{2}$/))
-      .optional(),
-    tags: z.array(z.string()).optional(),
-    slug: z.string().optional(),
-    cover: z.string().optional(),
-    draft: z.boolean().optional().default(false),
-  }),
+  schema: ({ image: img }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z
+        .string()
+        .datetime({ offset: true })
+        .or(z.string().regex(/^\d{4}-\d{2}-\d{2}$/))
+        .optional(),
+      tags: z.array(z.string()).optional(),
+      slug: z.string().optional(),
+      cover: img().optional(),
+      draft: z.boolean().optional().default(false),
+    }),
 });
 
 // Export collections
