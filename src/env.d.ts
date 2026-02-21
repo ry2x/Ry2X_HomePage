c890b2a3b547bca47df044bc0e0622edeb1981e9
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-02-21 09:04:25 +0000

    ♻️ REFAC: Make Env properties optional

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="astro/client" />
 
 interface Env {
-  RESEND_API_KEY: string;
-  TURNSTILE_SECRET_KEY: string;
-  CONTACT_TO_EMAIL: string;
-  CONTACT_FROM_EMAIL: string;
+  RESEND_API_KEY?: string;
+  TURNSTILE_SECRET_KEY?: string;
+  CONTACT_TO_EMAIL?: string;
+  CONTACT_FROM_EMAIL?: string;
 }
 
 type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
