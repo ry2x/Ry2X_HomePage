e21f2bedf39f44c04027987c2ffd9fea9aa6fb97
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-02-21 07:06:13 +0000

    ♻️ REFAC: Simplify Cloudflare environment variable structure

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,39 +1,31 @@
 /// <reference types="astro/client" />
 
-/**
- * Cloudflare 側の環境変数の定義
- */
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
 type CloudflareEnv = {
   RESEND_API_KEY: string;
   TURNSTILE_SECRET_KEY: string;
   CONTACT_TO_EMAIL: string;
   CONTACT_FROM_EMAIL: string;
 };
-
-declare global {
-  namespace App {
-    interface Locals {
-      runtime: {
-        env: CloudflareEnv;
-      };
-    }
-  }
-
-  interface ImportMetaEnv {
-    readonly PUBLIC_TURNSTILE_SITE_KEY: string;
-  }
-
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-
-  interface Window {
-    onTurnstileSuccess: (token: string) => void;
-    onTurnstileExpired: () => void;
-    turnstile?: {
-      reset: (widgetIdOrSelector?: string) => void;
-    };
+declare namespace App {
+  interface Locals {
+    runtime: Runtime<CloudflareEnv>;
   }
 }
 
-export {};
+interface ImportMetaEnv {
+  readonly PUBLIC_TURNSTILE_SITE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface Window {
+  onTurnstileSuccess: (token: string) => void;
+  onTurnstileExpired: () => void;
+  turnstile?: {
+    reset: (widgetIdOrSelector?: string) => void;
+  };
+}
